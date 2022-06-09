9e75aae7dbc6cb512d05e2d8991f9e03e673c1b0
Solwandle-Mihlali <mihlalisolwandle705@gmail.com>
2022-06-09 15:49:33 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+
+let user = prompt("enter selection").toLowerCase();
+if(user != "paper" && user != "rock" && user != "scissors" ){
+
+    alert("enter: rock, paper or scissors")
+}
+else{
+
+   playRound();
+}
+let computerSelection = function computerPlay(){
+
+    const randomArray = ["rock" , " paper" ," scissors"];
+    console.log (randomArray[(Math.random() * 3)]);
+}
+
+function playRound(user, computerSelection) { 
+    if (user === "rock") {
+        if (computerSelection === "rock") {
+            return "Draw!";
+        } else if (computerSelection === "paper") {
+            return "Computer wins!";
+        } else {
+            return "User wins!";
+        }
+    } else if (user === "paper") {
+        if (computerSelection === "rock") {
+            return "User wins!";
+        } else if (computerSelection === "paper") {
+            return "Draw!";
+        } else {
+            return "Computer wins!";
+        }
+    } else {
+        if (computerSelection === "rock") {
+            return "Computer wins!";
+        } else if (computerSelection === "paper") {
+            return "User wins!";
+        } else {
+            return "Draw!";
+        }
+    }
+}
+        
+console.log(playRound(user, computerSelection));
+
+
+
