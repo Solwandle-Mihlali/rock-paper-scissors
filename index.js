32d646ee645dc3968651938d5720361c80b4d4c6
MIhlali Solwandle <mihlali@aehdigital.co.za>
2022-06-20 11:59:45 +0200

    4th commit

--- index.js
+++ index.js
@@ -1,84 +1,32 @@
-let rock = document.getElementById('rock');
-let paper = document.getElementById('paper');
-let scissors = document.getElementById('scissors');
+const result = document.querySelector("#result");
+document.querySelectorAll("#opts a").forEach((a) =>
+  a.addEventListener("click", (e) => {
+    computerChoice(e);
+  })
+);
 
-rock = "rock";
-paper = "paper";
-scissors = "scissors"
+function computerChoice(e) {
+  let choices = ["Rock", "Paper", "Scissors"];
 
+  result.innerHTML = choices[Math.floor(Math.random() * choices.length)];
 
-function playRound(){
+  if (e.target.innerText.trim() == result.innerHTML) {
+    setTimeout(() => alert("Draw"), 100);
+  }
 
-    computerPlay();
+  if (
+    (e.target.innerText.trim() == "Rock" && result.innerHTML == "Scissors") ||
+    (e.target.innerText.trim() == "Paper" && result.innerHTML == "Rock") ||
+    (e.target.innerText.trim() == "Scissors" && result.innerHTML == "Paper")
+  ) {
+    setTimeout(() => alert("You win"), 100);
+  }
+
+  if (
+    (e.target.innerText.trim() == "Rock" && result.innerHTML == "Paper") ||
+    (e.target.innerText.trim() == "Paper" && result.innerHTML == "Scissors") ||
+    (e.target.innerText.trim() == "Scissors" && result.innerHTML == "Rock")
+  ) {
+    setTimeout(() => alert("Computer wins"), 100);
+  }
 }
-
-let compSelect = function computerPlay(){
-   
-    const randomArray = ["rock" , " paper" ," scissors"];
-    console.log (randomArray[(Math.random() * 3)]);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-let user = prompt("enter selection").toLowerCase();
-if(user != "paper" && user != "rock" && user != "scissors" ){
-
-    alert("enter: rock, paper or scissors")
-}
-else{
-
-   playRound();
-}
-let computerSelection = function computerPlay(){
-
-    const randomArray = ["rock" , " paper" ," scissors"];
-    console.log (randomArray[(Math.random() * 3)]);
-}
-
-function playRound(user, computerSelection) { 
-    if (user === "rock") {
-        if (computerSelection === "rock") {
-            return "Draw!";
-        } else if (computerSelection === "paper") {
-            return "Computer wins!";
-        } else {
-            return "User wins!";
-        }
-    } else if (user === "paper") {
-        if (computerSelection === "rock") {
-            return "User wins!";
-        } else if (computerSelection === "paper") {
-            return "Draw!";
-        } else {
-            return "Computer wins!";
-        }
-    } else {
-        if (computerSelection === "rock") {
-            return "Computer wins!";
-        } else if (computerSelection === "paper") {
-            return "User wins!";
-        } else {
-            return "Draw!";
-        }
-    }
-}
-        
-console.log(playRound(user, computerSelection));
-
-
-*/
-
